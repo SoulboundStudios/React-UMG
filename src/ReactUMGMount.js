2d65702223f3a35c4597901494dec692691804a6
Steve Hoelle <hoelle@gmail.com>
2017-10-23 18:53:00 -0700

    Namespace

--- src/ReactUMGMount.js
+++ src/ReactUMGMount.js
@@ -222,7 +222,7 @@ const ReactUMGMount = {
     return NodeMap[instance]
   }, 
   wrap(nextElement, outer = Root.GetEngine ? Unreal.JavascriptLibrary.CreatePackage(null,'/Script/Javascript') : GWorld) {
-    let widget = Root.GetEngine ? new Unreal.JavascriptWidget(outer) : GWorld.CreateWidget(JavascriptWidget);
+    let widget = Root.GetEngine ? new Unreal.JavascriptWidget(outer) : GWorld.CreateWidget(Unreal.JavascriptWidget);
     let instance = ReactUMGMount.render(nextElement, widget);
     return ReactUMGMount.findNode(instance) 
   }
